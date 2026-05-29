453b8585b38ef7285384ed5f301292221d7efae0
Pratiksha-gupta12 <142144827+Pratiksha-gupta12@users.noreply.github.com>
2026-05-29 10:53:58 +0530

    cart service

--- playwright.config.js
+++ playwright.config.js
@@ -25,13 +25,13 @@ export default defineConfig({
   // },
 
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 4 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html'],['allure-playwright', {resultsDir: 'allure-results' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
---
+++ tests/cart/add_cart.spec.js
@@ -0,0 +1,111 @@
+
+
+import { test, expect } from '@playwright/test';
+
+test.describe('Cart Service Tests', () => {
+
+test.beforeEach(async ({ page }) => {
+
+await page.goto('https://demo.nopcommerce.com/books');
+    await page.waitForLoadState(
+        'domcontentloaded'
+    );
+});
+
+
+test('TC_CART_001 ', async ({ page }) => {
+
+    await page.locator(
+        '.product-box-add-to-cart-button'
+    ).first().click();
+
+    await page.waitForTimeout(5000);
+
+    console.log('URL:', page.url());
+
+    console.log(
+        'Cart Qty:',
+        await page.locator('.cart-qty').textContent()
+    );
+});
+
+test(
+'TC_CART_002 - Add Multiple Products To Cart',
+async ({ page }) => {
+
+    const button1 =
+        page.locator(
+            '.product-box-add-to-cart-button'
+        ).nth(0);
+
+    const button2 =
+        page.locator(
+            '.product-box-add-to-cart-button'
+        ).nth(1);
+
+  
+    await expect(button1).toBeVisible();
+    await expect(button2).toBeVisible();
+
+    await button1.click();
+
+    
+    await page.waitForTimeout(3000);
+
+    await button2.click();
+   
+    await page.waitForTimeout(3000);
+
+    
+    await expect(button1).toBeVisible();
+    await expect(button2).toBeVisible();
+});
+
+test(
+'TC_CART_003 - Add Same Product Multiple Times',
+async ({ page }) => {
+
+    await page.goto(
+'https://demo.nopcommerce.com/fahrenheit-451-by-ray-bradbury'
+    );
+
+    const button =
+        page.locator(
+            'button[id^="add-to-cart-button"]'
+        );
+
+    await button.click();
+
+    await page.waitForTimeout(3000);
+
+    await button.click();
+
+    await page.waitForTimeout(3000);
+
+    console.log(
+        'Cart Qty:',
+        await page.locator('.cart-qty').textContent()
+    );
+
+    await expect(
+        page.locator('.cart-qty')
+    ).toBeVisible();
+});
+test(
+'TC_CART_004 - Add Product From Product Details Page',
+async ({ page }) => {
+
+    await page.locator('.product-title a')
+        .first()
+        .click();
+
+    await page.waitForLoadState('networkidle');
+
+    console.log('Current URL:', page.url());
+
+    await expect(
+        page.locator('h1')
+    ).toBeVisible();
+});
+});
+
